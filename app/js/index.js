058bc0752a5e8729ee2235012c4dc330f485fe38
Anatolii Bairov <tolikbairov16@gmail.com>
2022-01-30 12:46:16 +0200

    sidebar and footer nav logic

--- app/js/index.js
+++ app/js/index.js
@@ -1,46 +1,106 @@
 console.log("start work");
 document.addEventListener("DOMContentLoaded", function () {
-  console.log();
-
   if (window.innerWidth < 1024) {
     document.addEventListener("click", (e) => {
-      if (e.target.classList.contains("accordian_title")) {
-        let contentEl = e.target
-          .closest(".accordian")
-          .querySelector(".accordian_content");
+      /* footer nav listeners */
+      if (e.target.closest(".accordian_title")) {
+        let accordian = e.target.closest(".accordian");
+        let contentEl = accordian.querySelector(".accordian_content");
+
+        //check on already toggled element and close them
+        if (!contentEl.classList.contains("toggled")) {
+          toggledAccordianContent = document.querySelector(
+            ".accordian_content.toggled"
+          );
+          if (toggledAccordianContent) {
+            const toggledAccordian =
+              toggledAccordianContent.closest(".accordian");
+            if (toggledAccordian) {
+              accordianControls = toggledAccordian.querySelectorAll("button");
+
+              accordianControls.forEach((element) => {
+                element.classList.toggle("toggled");
+              });
+            }
+            toggledAccordianContent.classList.toggle("toggled");
+          }
+        }
         if (contentEl) {
+          accordianControls = accordian.querySelectorAll("button");
+
+          accordianControls.forEach((element) => {
+            element.classList.toggle("toggled");
+          });
           contentEl.classList.toggle("toggled");
           contentEl.scrollIntoView({
             behavior: "smooth",
             block: "start",
-            // inline: "nearest",
           });
         }
       }
-    });
-    // $(".accordian .accordian_title").click(function () {
-    //   $(this).parent().find(".accordian_content").slideToggle();
-    //   $(this).parent().siblings().find(".accordian_content").slideUp();
-    //   $(this).parent().toggleClass("active_header");
-    //   $(this).parent().siblings().removeClass("active_header");
-    // });
-  }
+      ///////
 
-  // Аналог $(document).ready(function(){
-  // Если должен быть найден один элемент
-  if (
-    (e = document.querySelector(
-      "#form_error_message_frontend + div > div:last-child label"
-    )) !== null
-  )
-    e.classList.add("last"); // Аналог выборки и присвоения класса
-  // Если элементов будет много
-  Array.prototype.forEach.call(
-    document.querySelectorAll(
-      "#form_error_message_frontend + div > div:last-child label"
-    ),
-    function (e) {
-      e.classList.add("last");
-    }
-  );
+      /**mobile search listeners */
+      if (e.target.classList.contains("toggle-search__icon")) {
+        let header = e.target.closest("header");
+        closeSearch = document.querySelector(".close-search__btn");
+        closeSearch.classList.toggle("show");
+        openSearch = document.querySelector(".open-search__btn");
+        openSearch.classList.toggle("show");
+        searchBar = header.querySelector(".header__search");
+        header.classList.toggle("search-active");
+
+        searchBar.classList.toggle("show");
+      }
+      //////
+    });
+  }
 });
+
+/**sidebar listener */
+const sidebarOpenBtn = document.querySelector("[data-open-sidebar]");
+const sidebarCloseBtn = document.querySelector("[data-close-sidebar]");
+const overflow = document.querySelector(".overflow");
+const sidebar = document.querySelector(".sidebar");
+
+const toggleSidebar = () => {
+  const body = document.querySelector("body");
+  body.classList.toggle("hideScroll");
+  sidebar.classList.toggle("show");
+  overflow.classList.toggle("show");
+};
+overflow.addEventListener("click", () => {
+  toggleSidebar();
+});
+sidebarOpenBtn.addEventListener("click", () => {
+  toggleSidebar();
+});
+sidebarCloseBtn.addEventListener("click", () => {
+  toggleSidebar();
+});
+sidebar.addEventListener("click", (e) => {
+  // console.log(e.target.closest(""));
+  if (e.target.closest(".btn-prev")) {
+    console.log("click-prev");
+    const currentNav = e.target.closest(".sidebar__nav");
+    const prevNavID = currentNav.dataset.prevNav;
+
+    const prevNav = document.getElementById(prevNavID);
+    if (prevNav) {
+      currentNav.classList.toggle("show");
+      prevNav.classList.toggle("show");
+      console.log("prevNav");
+    }
+  }
+  if (e.target.closest("[data-next-nav]")) {
+    const currentNav = e.target.closest(".sidebar__nav");
+    const nextNavID = e.target.closest("[data-next-nav]").dataset.nextNav;
+    const nextNav = document.getElementById(nextNavID);
+    if (nextNav) {
+      currentNav.classList.toggle("show");
+      nextNav.classList.toggle("show");
+    }
+  }
+});
+const sidebarNavigarton = {};
+///////
